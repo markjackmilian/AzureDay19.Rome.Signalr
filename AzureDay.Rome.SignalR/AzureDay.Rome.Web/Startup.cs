cbab53bfc561c54263e6917285aa7cabfba51929
Marco Milani <emme.milani@hotmail.it>
2019-05-04 15:57:44 +0200

    Add default routes and static files support

--- AzureDay.Rome.SignalR/AzureDay.Rome.Web/Startup.cs
+++ AzureDay.Rome.SignalR/AzureDay.Rome.Web/Startup.cs
@@ -24,8 +24,11 @@ namespace AzureDay.Rome.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            //app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
         }
     }
 }
